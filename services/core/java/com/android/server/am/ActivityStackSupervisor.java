aosp-mirror/platform_frameworks_base

commit 2d0f39bf33b9f3dc90ce4d8bc34d59e00a06658d

Move home task to front when home stack is moved to front.
Make sure the home task is moved to the front when we are moving
the home stack to the front for HOME_ACTIVITY_TYPE task type.

Bug: 20323082
Change-Id: I035cc86c48633089086f61d3a890ff87b4091479

services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -462,7 +462,7 @@ public final class ActivityStackSupervisor implements DisplayListener {
 
         mHomeStack.moveHomeStackTaskToTop(homeStackTaskType);
 
-        final ActivityRecord top = mHomeStack.topRunningActivityLocked(null);
+        final ActivityRecord top = getHomeActivity();
         if (top == null) {
             return false;
         }
@@ -485,7 +485,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
             prev.task.setTaskToReturnTo(APPLICATION_ACTIVITY_TYPE);
         }
 
-        ActivityRecord r = mHomeStack.topRunningActivityLocked(null);
+        mHomeStack.moveHomeStackTaskToTop(homeStackTaskType);
+        ActivityRecord r = getHomeActivity();
         if (r != null) {
             mService.setFocusedActivityLocked(r, reason);
             return resumeTopActivitiesLocked(mHomeStack, prev, null);

services/core/java/com/android/server/am/TaskRecord.java
@@ -385,10 +385,8 @@ final class TaskRecord {
     }
 
     void setTaskToReturnTo(int taskToReturnTo) {
-        if (IGNORE_RETURN_TO_RECENTS && taskToReturnTo == RECENTS_ACTIVITY_TYPE) {
-            taskToReturnTo = HOME_ACTIVITY_TYPE;
-        }
-        mTaskToReturnTo = taskToReturnTo;
+        mTaskToReturnTo = (IGNORE_RETURN_TO_RECENTS && taskToReturnTo == RECENTS_ACTIVITY_TYPE)
+            ? HOME_ACTIVITY_TYPE : taskToReturnTo;
     }
 
     int getTaskToReturnTo() {

khadas/android_frameworks_base

commit c70499d3f49c237417a6671c97d08239e060650a

pd #116834 use topRunningActivityLocked replace getHomeActivity
this issue caused by 2d0f39bf33b9f3dc90ce4d8bc34d59e00a06658d

Change-Id: I311e379a39fcf580fced4c083b014f4fb6ad3bff

services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -486,7 +486,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
 
         mHomeStack.moveHomeStackTaskToTop(homeStackTaskType);
 
-        final ActivityRecord top = getHomeActivity();
+        //final ActivityRecord top = getHomeActivity();
+        final ActivityRecord top = mHomeStack.topRunningActivityLocked(null);
         if (top == null) {
             return false;
         }
@@ -510,7 +511,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
         }
 
         mHomeStack.moveHomeStackTaskToTop(homeStackTaskType);
-        ActivityRecord r = getHomeActivity();
+        //ActivityRecord r = getHomeActivity();
+        ActivityRecord r = mHomeStack.topRunningActivityLocked(null);
         if (r != null) {
             mService.setFocusedActivityLocked(r, reason);
             return resumeTopActivitiesLocked(mHomeStack, prev, null);