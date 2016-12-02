6d880cdfb4276214ea0ef1ba9952b59dcead3fce
Hans Yang <yhx@rock-chips.com>
2016-12-02 20:46:32 +0800

    KeyguardService: if is box product, don't bind keyguard service
    Change-Id: Iec42018cc0127bc91f4aefb09a799e3e5c0c5e6a
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- services/core/java/com/android/server/policy/keyguard/KeyguardServiceDelegate.java
+++ services/core/java/com/android/server/policy/keyguard/KeyguardServiceDelegate.java
@@ -122,6 +122,12 @@ public class KeyguardServiceDelegate {
         mScrim = createScrim(context, mScrimHandler);
     }
 
+    private boolean isBox() {
+        String boxString = android.os.SystemProperties.get("ro.target.product");
+        boolean isBox = "box".equals(boxString);
+        return isBox;
+    }
+
     public void bindService(Context context) {
         Intent intent = new Intent();
         final Resources resources = context.getApplicationContext().getResources();
@@ -131,7 +137,9 @@ public class KeyguardServiceDelegate {
         intent.addFlags(Intent.FLAG_DEBUG_TRIAGED_MISSING);
         intent.setComponent(keyguardComponent);
 
-        if (!context.bindServiceAsUser(intent, mKeyguardConnection,
+        boolean isBox = isBox();
+
+        if (isBox || !context.bindServiceAsUser(intent, mKeyguardConnection,
                 Context.BIND_AUTO_CREATE, mScrimHandler, UserHandle.SYSTEM)) {
             Log.v(TAG, "*** Keyguard: can't bind to " + keyguardComponent);
             mKeyguardState.showing = false;
