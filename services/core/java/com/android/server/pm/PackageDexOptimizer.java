21d43d9d5c6e996a007c97448d5336c29d866946
Hao Xiaowei <hxw@rock-chips.com>
2016-12-21 10:37:13 +0800

    cts: Add property for cts & gts test
    Change-Id: Ife48d0a8ba70e659b267d07791fdeb9d2f119792
    Signed-off-by: Hao Xiaowei <hxw@rock-chips.com>

--- services/core/java/com/android/server/pm/PackageDexOptimizer.java
+++ services/core/java/com/android/server/pm/PackageDexOptimizer.java
@@ -23,6 +23,7 @@ import android.content.pm.PackageParser;
 import android.os.Environment;
 import android.os.PowerManager;
 import android.os.UserHandle;
+import android.os.SystemProperties;
 import android.os.WorkSource;
 import android.util.Log;
 import android.util.Slog;
@@ -245,6 +246,9 @@ class PackageDexOptimizer {
                         + " vmSafeMode=" + vmSafeMode + " debuggable=" + debuggable
                         + " target-filter=" + targetCompilerFilter + " oatDir = " + oatDir
                         + " sharedLibraries=" + sharedLibrariesPath);
+                if(pkg.applicationInfo.packageName.contains("com.android.cts")||pkg.applicationInfo.packageName.contains("com.google.android.xts")){
+                    SystemProperties.set("sys.cts_gts.status","true");
+                }
                 // Profile guide compiled oat files should not be public.
                 final boolean isPublic = !pkg.isForwardLocked() && !isProfileGuidedFilter;
                 final int profileFlag = isProfileGuidedFilter ? DEXOPT_PROFILE_GUIDED : 0;
