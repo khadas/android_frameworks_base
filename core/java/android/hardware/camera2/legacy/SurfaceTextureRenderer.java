6828dd1b817017ac7c7bbe66bfda1b7eb09843ac
Hao Xiaowei <hxw@rock-chips.com>
2017-05-19 09:51:57 +0800

    Revert "camera cts: Catch more exception for surface abandonend"
    This reverts commit cb5f7b59d017daee6e81dcf8243f5c9facc1ec41.
    
    Change-Id: I1a61b9b210bb00f556359e86fcc1451387a4fe45

--- core/java/android/hardware/camera2/legacy/SurfaceTextureRenderer.java
+++ core/java/android/hardware/camera2/legacy/SurfaceTextureRenderer.java
@@ -751,9 +751,8 @@ public class SurfaceTextureRenderer {
                             (mFacing == CameraCharacteristics.LENS_FACING_FRONT) ?
                                     FLIP_TYPE_HORIZONTAL : FLIP_TYPE_NONE);
                     swapBuffers(holder.eglSurface);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Log.e(TAG, "Surface abandoned, dropping frame. ", e);
+                } catch (LegacyExceptionUtils.BufferQueueAbandonedException e) {
+                    Log.w(TAG, "Surface abandoned, dropping frame. ", e);
                     request.setOutputAbandoned();
                 }
             }
