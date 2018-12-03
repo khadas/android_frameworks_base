92dadb50ade9b98875bd02ed323efb442cca97eb
alex.lin <alex.lin@rock-chips.com>
2018-12-03 09:33:02 +0800

    tablet: if ro.vendor.udisk.visible is true ,for all public volume is visible
    https://redmine.rockchip.com.cn/issues/185437
    
    Change-Id: Ia655f8ed3ae37bef591ba999e0a18b5b317f431c
    Signed-off-by: alex.lin <alex.lin@rock-chips.com>

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -1183,7 +1183,8 @@ class StorageManagerService extends IStorageManager.Stub
             }
 
             if(("box".equals(SystemProperties.get("ro.target.product","unknown"))
-               ||"atv".equals(SystemProperties.get("ro.target.product","unknown")))
+               ||"atv".equals(SystemProperties.get("ro.target.product","unknown"))
+               ||"tablet".equals(SystemProperties.get("ro.target.product","unknown")))
                && "true".equals(SystemProperties.get("ro.vendor.udisk.visible"))){
                 Log.d(TAG,"-----for all public volume is visible-----");
                 vol.mountFlags |= VolumeInfo.MOUNT_FLAG_VISIBLE;
