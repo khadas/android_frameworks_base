afbc5bf953ee5399e589c901abe53b5aef505259
Jinping Wang <jinping.wang@amlogic.com>
2019-05-22 17:13:18 +0800

    cec: skip Intent.ACTION_SHUTDOWN when reboot [1/1]
    PD# SWPL-8666
    
    Problem:
    HdmiControlService will receive Intent.ACTION_SHUTDOWN
    when reboot, it may lead to send <Standby>
    
    Solution:
    skip Intent.ACTION_SHUTDOWN when reboot
    
    Verify:
    test ok using p321
    
    Change-Id: I49b8ea667d163f6bb7cd0a5b3ea6b3f4b0c0b238
    Signed-off-by: Jinping Wang <jinping.wang@amlogic.com>

--- services/core/java/com/android/server/hdmi/HdmiControlService.java
+++ services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -24,6 +24,7 @@ import static com.android.server.hdmi.Constants.OPTION_MHL_ENABLE;
 import static com.android.server.hdmi.Constants.OPTION_MHL_INPUT_SWITCHING;
 import static com.android.server.hdmi.Constants.OPTION_MHL_POWER_CHARGE;
 import static com.android.server.hdmi.Constants.OPTION_MHL_SERVICE_CONTROL;
+import static com.android.server.power.ShutdownThread.SHUTDOWN_ACTION_PROPERTY;
 
 import android.annotation.Nullable;
 import android.content.BroadcastReceiver;
@@ -145,6 +146,7 @@ public final class HdmiControlService extends SystemService {
         @Override
         public void onReceive(Context context, Intent intent) {
             assertRunOnServiceThread();
+            boolean isReboot = SystemProperties.get(SHUTDOWN_ACTION_PROPERTY).contains("1");
             switch (intent.getAction()) {
                 case Intent.ACTION_SCREEN_OFF:
                     if (isPowerOnOrTransient()) {
@@ -171,7 +173,7 @@ public final class HdmiControlService extends SystemService {
                     }
                     break;
                 case Intent.ACTION_SHUTDOWN:
-                    if (isPowerOnOrTransient()) {
+                    if (isPowerOnOrTransient() && !isReboot) {
                         onStandby(STANDBY_SHUTDOWN);
                     }
                     break;
