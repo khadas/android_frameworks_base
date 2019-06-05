5efeb33efab4ac42e47abac16dd1b1ba491bd638
xian <an.xi@amlogic.com>
2019-06-05 08:19:25 -0400

    TV: fix memory leak [1/2]
    PD#SWPL-9378
    
    Problem:
    memory leak happens in MainActivity
    
    Solution:
    remove the listener which holds the reference of MainActivity when destroyed
    
    Verify:
    verify it on Ampere
    
    Change-Id: I7edb27eac557c23310bad527057abd79f60a7535
    Signed-off-by: an.xi <an.xi@amlogic.com>

--- services/core/java/com/android/server/hdmi/HdmiControlService.java
+++ services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -1445,6 +1445,12 @@ public final class HdmiControlService extends SystemService {
         }
 
         @Override
+        public void removeInputChangeListener(final IHdmiInputChangeListener listener) {
+            enforceAccessPermission();
+            HdmiControlService.this.removeInputChangeListener(listener);
+        }
+
+        @Override
         public List<HdmiDeviceInfo> getInputDevices() {
             enforceAccessPermission();
             // No need to hold the lock for obtaining TV device as the local device instance
@@ -1854,6 +1860,8 @@ public final class HdmiControlService extends SystemService {
 
     private void setInputChangeListener(IHdmiInputChangeListener listener) {
         synchronized (mLock) {
+            // Todo
+            // For now there can only be one listener to maintain.
             mInputChangeListenerRecord = new InputChangeListenerRecord(listener);
             try {
                 listener.asBinder().linkToDeath(mInputChangeListenerRecord, 0);
@@ -1861,6 +1869,22 @@ public final class HdmiControlService extends SystemService {
                 Slog.w(TAG, "Listener already died");
                 return;
             }
+        }
+    }
+
+    private void removeInputChangeListener(IHdmiInputChangeListener listener) {
+        synchronized (mLock) {
+            Slog.d(TAG, "removeInputChangeListener");
+            if (null == listener) {
+                return;
+            }
+            try {
+                listener.asBinder().unlinkToDeath(mInputChangeListenerRecord, 0);
+            } catch (Exception e) {
+                Slog.w(TAG, "Listener already died");
+                return;
+            }
+            mInputChangeListenerRecord = null;
         }
     }
 
