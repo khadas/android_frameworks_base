e307c9cf2b9285aa7aab45f8c403a0291aad7c4d
Bin Yang <yangbin@rock-chips.com>
2019-08-20 14:04:14 +0800

    UsbHostManager: UsbHostServer does not need operate NPU rockusb
    Rockchip rockusb devices can only be used for image upgrade, so
    UsbHostServer should not operate it.
    
    Change-Id: Iea613adc27e8201411c4f8862476338e1a6ed465
    Signed-off-by: Bin Yang <yangbin@rock-chips.com>

--- services/core/jni/com_android_server_UsbHostManager.cpp
+++ services/core/jni/com_android_server_UsbHostManager.cpp
@@ -28,10 +28,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-
 #include <usbhost/usbhost.h>
+#include <dirent.h>
+#include <ctype.h>
 
 #define MAX_DESCRIPTORS_LENGTH 4096
+
+#define USB_DEV_DIR     "/sys/bus/usb/devices"
+#define USB_DEV_PID     "idProduct"
+#define USB_DEV_VID     "idVendor"
+#define USB_DEV_NUM     "devnum"
+
+enum value_type {
+    ATTR_VAL_HEX = 0,
+    ATTR_VAL_DEC = 1,
+};
 
 namespace android
 {
@@ -53,7 +64,169 @@ static void checkAndClearExceptionFromCallback(JNIEnv* env, const char* methodNa
     }
 }
 
+static int str_to_digit(const char *string, enum value_type type)
+{
+    int value = 0;
+    char buf[100];
+
+    strncpy(buf, string, sizeof(buf));
+    if (type == ATTR_VAL_HEX) {
+        while(*string) {
+            if(!isxdigit(*string++))
+                return -1;
+        }
+        if (sscanf(buf, "%x", &value) < 1)
+            return -1;
+    } else {
+        while(*string) {
+            if(!isdigit(*string++))
+                return -1;
+        }
+        value = atoi(buf);
+    }
+
+    return value;
+}
+
+static char* usb_device_get_attr_str(const char *devpath, const char *attr)
+{
+    char buf[100];
+    char node_name[50];
+    FILE *fp;
+    char str_len;
+
+    snprintf(node_name, sizeof(node_name), "%s/%s", devpath, attr);
+    fp = fopen(node_name, "r");
+    if (fp == NULL) {
+        ALOGE("Failed to open %s", node_name);
+        return NULL;
+    }
+
+    if (!fgets(buf, sizeof(buf), fp)) {
+        ALOGE("Failed to get  %s", node_name);
+        fclose(fp);
+        return NULL;
+    }
+
+    str_len = strlen(buf);
+    if (buf[str_len-1] == '\n')
+        buf[str_len-1] = '\0';
+
+    fclose(fp);
+
+    return strdup(buf);
+}
+
+static int usb_device_get_attr_val(const char *devpath, const char *attr,
+				   enum value_type type)
+{
+    char *attr_str;
+    int value = 0;
+
+    attr_str = usb_device_get_attr_str(devpath, attr);
+    if (!attr_str)
+        return -1;
+
+    value = str_to_digit(attr_str, type);
+    if (value < 0)
+        ALOGE("\"%s\" is illegal string!", attr_str);
+
+    free(attr_str);
+
+    return value;
+}
+
+static char* usb_device_get_attr_str_from_name(const char *devname,
+					       const char *attr)
+{
+    int id;
+    int bus;
+    int devnum;
+    char portname[5];
+    char subportname[5];
+    char portdir[50];
+    DIR *devdir;
+    struct dirent *de;
+
+    id = usb_device_get_unique_id_from_name(devname);
+    bus = id / 1000;
+    devnum = id % 1000;
+
+    snprintf(subportname, sizeof(subportname), "%d-1", bus);
+    snprintf(portname, sizeof(portname), "usb%d", bus);
+
+    devdir = opendir(USB_DEV_DIR);
+    if(devdir == 0) {
+        ALOGE ("%s does not exist!", USB_DEV_DIR);
+        return NULL;
+    }
+
+    while ((de = readdir(devdir))) {
+        if ((!strncmp(de->d_name, subportname, 3) &&
+             !strchr(de->d_name, ':')) ||
+            !strncmp(de->d_name, portname, 4)) {
+
+            snprintf(portdir, sizeof(portdir), USB_DEV_DIR "/%s", de->d_name);
+            if (devnum == usb_device_get_attr_val(portdir, USB_DEV_NUM,
+                                                  ATTR_VAL_DEC)) {
+                closedir(devdir);
+                return usb_device_get_attr_str(portdir, attr);
+            }
+        }
+    }
+    closedir(devdir);
+    ALOGE ("%s do not have usb device!", devname);
+
+    return NULL;
+}
+
+static int usb_device_get_attr_val_from_name(const char *devname,
+					     const char *attr,
+					     enum value_type type)
+{
+    char *attr_str;
+    int value = 0;
+
+    attr_str = usb_device_get_attr_str_from_name(devname, attr);
+    if (!attr_str)
+        return -1;
+
+    value = str_to_digit(attr_str, type);
+    if (value < 0)
+        ALOGE("\"%s\" is illegal string!", attr_str);
+    free(attr_str);
+
+    return value;
+}
+
+static int usb_device_is_rockusb(const char *devname)
+{
+    int idProduct = 0;
+    int idVendor = 0;
+
+    idProduct = usb_device_get_attr_val_from_name(devname, USB_DEV_PID, ATTR_VAL_HEX);
+    idVendor = usb_device_get_attr_val_from_name(devname, USB_DEV_VID, ATTR_VAL_HEX);
+
+
+    if (idVendor < 0 || idProduct < 0) {
+        ALOGE ("Get USB device idVendor or idProduct failed");
+        return 0;
+    }
+
+    if (idVendor == 0x2207 && idProduct == 0x180a) {
+        ALOGE ("USB device (idVendor:0x%4x idProduct:0x%4x) is NPU rockusb", idVendor, idProduct);
+        return 1;
+    }
+
+    return 0;
+}
+
 static int usb_device_added(const char *devAddress, void* clientData) {
+    if (usb_device_is_rockusb(devAddress)) {
+        ALOGE("%s is Rockchip rockusb, don't open it!", devAddress);
+        return 0;
+    }
+
     struct usb_device *device = usb_device_open(devAddress);
     if (!device) {
         ALOGE("usb_device_open failed\n");
