997a8676b828a0d3720ab842afae548371df0ca4
wenbiao zhang <wenbiao.zhang@amlogic.com>
2019-10-21 17:09:57 +0800

    cec: fix no sound for GIEC DVD after connect/disconnect bt speaker [1/1]
    PD#SWPL-12924
    
    Problem:
    no sound for GIEC DVD after connect/disconnect bt speaker
    
    Solution:
    check hdmi port for avr device
    
    Verify:
    marconi
    
    Change-Id: Ic0b136ee938f65706a7a8eeefdbb5876ebc7c8f4
    Signed-off-by: wenbiao zhang <wenbiao.zhang@amlogic.com>

--- services/core/java/com/android/server/hdmi/HdmiCecLocalDeviceTv.java
+++ services/core/java/com/android/server/hdmi/HdmiCecLocalDeviceTv.java
@@ -1014,6 +1014,11 @@ final class HdmiCecLocalDeviceTv extends HdmiCecLocalDevice {
             //mSystemAudioActivated = on;
             return;
         }
+        HdmiDeviceInfo avr = getSafeAvrDeviceInfo();
+        if (avr == null || !isConnectedToArcPort(avr.getPhysicalAddress())) {
+            return;
+        }
+
         updateAudioManagerForSystemAudio(on);
         synchronized (mLock) {
             if (!hasAction(RequestShortAudioDescriptorAction.class) && on) {
