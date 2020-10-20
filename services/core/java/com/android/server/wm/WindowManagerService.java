a0a8e055401f84c6caee0872ef3b384a5686d57a
Alex Wang <alex.wang@rock-chips.com>
2020-10-20 16:09:21 +0800

    Box:skip FallbackHome to fix 2-3s black screen before launcher.
    Change-Id: If815c9598db86e2173d6764c4dc5948b8cff6e6d

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3579,6 +3579,8 @@ public class WindowManagerService extends IWindowManager.Stub
                 }
             }
 
+        //box:skip FallbackHome to fix 2-3s black screen before launcher.
+        if (!"box".equals(SystemProperties.get("ro.target.product"))) {
             if (!mBootAnimationStopped) {
                 Trace.asyncTraceBegin(TRACE_TAG_WINDOW_MANAGER, "Stop bootanim", 0);
                 // stop boot animation
@@ -3606,6 +3608,7 @@ public class WindowManagerService extends IWindowManager.Stub
             } catch (RemoteException ex) {
                 ProtoLog.e(WM_ERROR, "Boot completed: SurfaceFlinger is dead!");
             }
+        }
 
             EventLogTags.writeWmBootAnimationDone(SystemClock.uptimeMillis());
             Trace.asyncTraceEnd(TRACE_TAG_WINDOW_MANAGER, "Stop bootanim", 0);
