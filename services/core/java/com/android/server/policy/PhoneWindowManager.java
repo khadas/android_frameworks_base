6a8b27632964267743217c85076180423e08e249
jason <jason@khadas.com>
2021-10-14 07:23:36 -0400

    Navigation: fixup Navigation bar display problem when chagne density
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I7fceae5710c17f1110988affc01e26dc894cb522

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2408,7 +2408,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         int longSizeDp = longSize * DisplayMetrics.DENSITY_DEFAULT / density;
 
         // Allow the navigation bar to move on non-square small devices (phones).
-        mNavigationBarCanMove = width != height && shortSizeDp < 600;
+        //mNavigationBarCanMove = width != height && shortSizeDp < 600;
 
         mHasNavigationBar = res.getBoolean(com.android.internal.R.bool.config_showNavigationBar);
 
