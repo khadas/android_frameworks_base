f151ec92ae527bdaef29797120b92a20ab3084a7
Goenjoy Huang <goenjoy@khadas.com>
2022-05-17 21:15:59 +0000

    Ethernet: add interface for settings apk
    Signed-off-by: Goenjoy Huang <goenjoy@khadas.com>

--- core/java/android/net/EthernetManager.java
+++ core/java/android/net/EthernetManager.java
@@ -145,6 +145,54 @@ public class EthernetManager {
     }
 
     /**
+     * @hide
+     */
+    @UnsupportedAppUsage
+    public String getIpAddress(String iface) {
+        try {
+            return mService.getIpAddress(iface);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * @hide
+     */
+    @UnsupportedAppUsage
+    public String getNetmask(String iface) {
+        try {
+            return mService.getNetmask(iface);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * @hide
+     */
+    @UnsupportedAppUsage
+    public String getGateway(String iface) {
+        try {
+            return mService.getGateway(iface);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * @hide
+     */
+    @UnsupportedAppUsage
+    public String getDns(String iface) {
+        try {
+            return mService.getDns(iface);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
      * Adds a listener.
      * @param listener A {@link Listener} to add.
      * @throws IllegalArgumentException If the listener is null.
