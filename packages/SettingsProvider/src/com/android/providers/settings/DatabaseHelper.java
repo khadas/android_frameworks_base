0b70b27beb7748b424b7218f97bd56f531e4929a
wh <wanghang@rock-chips.com>
2023-12-20 14:51:18 +0800

    add def_animator_duration_scale cfg for ANIMATOR_DURATION_SCALE
    Change-Id: I7cc804f600a3c309c3e68c6d952d64885605b779
    Signed-off-by: wh <wanghang@rock-chips.com>

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2292,6 +2292,10 @@ class DatabaseHelper extends SQLiteOpenHelper {
                 R.fraction.def_window_animation_scale, 1);
         loadFractionSetting(stmt, Settings.System.TRANSITION_ANIMATION_SCALE,
                 R.fraction.def_window_transition_scale, 1);
+        //-----------------------rk code----------
+        loadFractionSetting(stmt, Settings.System.ANIMATOR_DURATION_SCALE,
+                R.fraction.def_animator_duration_scale, 1);
+        //----------------------------------------
     }
 
     private void loadDefaultHapticSettings(SQLiteStatement stmt) {
