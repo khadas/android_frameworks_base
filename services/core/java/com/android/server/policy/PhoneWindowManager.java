373c7ac10359eacc1c40ca89cd55eb2cdb718418
Xiong Zhang <xiong.zhang@wesion.com>
2024-07-06 16:13:56 +0800

    key: add function key
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3107,6 +3107,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 	//----------------------------------------
 
+        //add function key
+        if(keyCode == KeyEvent.KEYCODE_HOME){
+            if(SystemProperties.getInt("persist.sys.func.key.action", 3) != 3){
+                Intent intentkey = new Intent("com.customer.key");
+                intentkey.putExtra("code",keyCode);
+                mContext.sendBroadcast(intentkey);
+                return -1;
+            }
+        }
+        //add end
+
         if (mKeyCombinationManager.isKeyConsumed(event)) {
             return keyConsumed;
         }
