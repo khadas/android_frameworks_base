82fc4c0fd2b84f0298ca0ad8620e6b5320bd2a07
shine.liu <shine.liu@rock-chips.com>
2024-07-17 16:15:39 +0800

    AudioService: Synchronize the volume of hdmi/spdif/speaker
    Type: Fix
    Redmine ID: #496194
    Associated modifications: N/A
    Test: N/A
    
    Signed-off-by: shine.liu <shine.liu@rock-chips.com>
    Change-Id: I0c40af03a84963e6d352db5a051a764e0a21401d

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -708,7 +708,13 @@ public class AudioService extends IAudioService.Stub
             AudioSystem.DEVICE_OUT_HDMI_ARC,
             AudioSystem.DEVICE_OUT_HDMI_EARC
     ));
-
+    // Devices for which the volumes are being ajusted at the same time
+    Set<Integer> mSyncAjustVolumeDevices = new HashSet<>(Arrays.asList(
+        AudioSystem.DEVICE_OUT_HDMI,
+        AudioSystem.DEVICE_OUT_HDMI_1,
+        AudioSystem.DEVICE_OUT_SPDIF,
+        AudioSystem.DEVICE_OUT_SPDIF_1,
+        AudioSystem.DEVICE_OUT_SPEAKER));
     // Devices where the framework sends a full scale audio signal, and controls the volume of
     // the external audio system separately.
     // For possible volume behaviors, see {@link AudioManager.AbsoluteDeviceVolumeBehavior}.
@@ -8670,7 +8676,14 @@ public class AudioService extends IAudioService.Stub
             } else {
                 index = (getIndex(device) + 5)/10;
             }
-            setStreamVolumeIndex(index, device);
+            if (isTablet() && isSyncAjustVolumeDevice(device) && mStreamType == AudioSystem.STREAM_MUSIC) {
+                for (Integer item : mSyncAjustVolumeDevices) {
+                    index = (getIndex(item) + 5)/10;
+                    setStreamVolumeIndex(index, item);
+                }
+            } else {
+                setStreamVolumeIndex(index, device);
+            }
         }
 
         public void applyAllVolumes() {
@@ -8720,14 +8733,36 @@ public class AudioService extends IAudioService.Stub
                     hasModifyAudioSettings);
         }
 
+        //save the volume of all the devices in mSyncAjustVolumeDevices
+        public void restoreSyncDevicesIndex(){
+            if(mStreamType == AudioSystem.STREAM_MUSIC){
+                for (Integer device : mSyncAjustVolumeDevices) {
+                    System.putIntForUser(mContentResolver,
+                          getSettingNameForDevice(device),
+                         (getIndex(device) + 5)/ 10,
+                          UserHandle.USER_CURRENT);
+                }
+            }
+        }
+
         public boolean setIndex(int index, int device, String caller,
                 boolean hasModifyAudioSettings) {
             boolean changed;
             int oldIndex;
+            int deltaIndex;
             final boolean isCurrentDevice;
             synchronized (mSettingsLock) {
                 synchronized (VolumeStreamState.class) {
                     oldIndex = getIndex(device);
+                    deltaIndex = index - oldIndex;
+                    if (isTablet() && isSyncAjustVolumeDevice(device) && mStreamType == AudioSystem.STREAM_MUSIC) {
+                        int index_tmp;
+                        for (Integer item : mSyncAjustVolumeDevices) {
+                            index_tmp = getIndex(item) + deltaIndex;
+                            index_tmp = getValidIndex(index_tmp, hasModifyAudioSettings);
+                            mIndexMap.put(item, index_tmp);
+                        }
+                    }
                     index = getValidIndex(index, hasModifyAudioSettings);
                     if ((mStreamType == AudioSystem.STREAM_SYSTEM_ENFORCED) && mCameraSoundForced) {
                         index = mIndexMax;
@@ -11379,6 +11414,7 @@ public class AudioService extends IAudioService.Stub
         pw.print("  mNotifAliasRing="); pw.println(mNotifAliasRing);
         pw.print("  mFixedVolumeDevices="); pw.println(dumpDeviceTypes(mFixedVolumeDevices));
         pw.print("  mFullVolumeDevices="); pw.println(dumpDeviceTypes(mFullVolumeDevices));
+        pw.print("  mSyncAjustVolumeDevices="); pw.println(dumpDeviceTypes(mSyncAjustVolumeDevices));
         pw.print("  absolute volume devices="); pw.println(dumpDeviceTypes(
                 getAbsoluteVolumeDevicesWithBehavior(
                         AudioManager.DEVICE_VOLUME_BEHAVIOR_ABSOLUTE)));
@@ -13478,6 +13514,14 @@ public class AudioService extends IAudioService.Stub
             return false;
         }
         return mFullVolumeDevices.contains(deviceType);
+    }
+
+    private boolean isSyncAjustVolumeDevice(int deviceType) {
+        if (deviceType == AudioSystem.DEVICE_OUT_REMOTE_SUBMIX
+                && mRecordMonitor.isLegacyRemoteSubmixActive()) {
+            return false;
+        }
+        return mSyncAjustVolumeDevices.contains(deviceType);
     }
 
     /**
